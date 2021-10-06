233df80f180323082d260c04ccfd73c41244a61c
Swathi <swathi.h@perfios.com>
2021-10-06 18:34:04 +0530

    ADDED: Using array helper functions on working days of an employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -35,3 +35,62 @@ while(totalEmployeeHours<=MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORING
 }
 let employeeWage = calclateDailyWage(totalEmployeeHours);
 console.log("Total Days: " + totalWorkingDays + " Total Hours: " + totalEmployeeHours + " Employee Wage: " + employeeWage);
+
+//UC7A
+let totalWage=0;
+function sum(dailyWage) {
+    totalWage += dailyWage;
+}
+empDailyWageArr.forEach(sum);
+console.log("UC7A - Total Days: " + totalWorkingDays + " Total hours: "
+        + totalEmployeeHours + " Emp Wage: " + totalWage);
+
+function totalWages(totWage,dailyWage) {
+    return totWage + dailyWage;
+}
+
+console.log("UC7A using reduce " + empDailyWageArr.reduce(totalWages, 0));
+
+//UC7B
+let dailyCounter = 0;
+function mapDayWithWage(dailyWage) {
+    dailyCounter++;
+    return dailyCounter + " = " + dailyWage;
+}
+let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
+console.log("UC7B - Daily Wage map");
+console.log(mapDayWithWageArr);
+
+//UC7C
+function fulltimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+let fullDayWageArr = mapDayWithWageArr.filter(fulltimeWage);
+console.log("UC7C - Daily wge filter when full time wage earned");
+console.log(fullDayWageArr);
+
+//UC7D
+function findFullTimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+console.log("UC7D - first full time wage was earned on: " + mapDayWithWageArr.find(findFullTimeWage));
+
+//UC7E
+function isAllFullTimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+console.log("UC7E - check if all elements have full time wage: " + fullDayWageArr.every(isAllFullTimeWage));
+
+//UC7F
+function isAnyPartTimeWage(dailyWage) {
+    return dailyWage.includes("80");
+}
+console.log("UC7F - Check if any part time wage: " + mapDayWithWageArr.some(isAnyPartTimeWage));
+
+//UC7G
+function totalDaysWorked(numOfDays, dailyWage) {
+    if(dailyWage > 0) 
+        return numOfDays + 1;
+    return numOfDays;
+}
+console.log("UC7G - Number of Days Worked: " + empDailyWageArr.reduce(totalDaysWorked,0));
